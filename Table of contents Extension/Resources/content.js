29d3bb9bd87ef81e4c0813d70f467a94962a90b9
Nathan Manceaux-Panot <nathan@lemon.garden>
2023-01-09 19:38:39 +0100

    Implement basic functionnality

--- Table of contents Extension/Resources/content.js
+++ Table of contents Extension/Resources/content.js
@@ -1,7 +1,38 @@
-browser.runtime.sendMessage({ greeting: "hello" }).then((response) => {
-    console.log("Received response: ", response);
-});
+const knownMessages = {
+	getHeadingInfos(request, sender, sendResponse) {
+		sendResponse(getHeadingInfos());
+	},
+	revealHeading(request, sender, sendResponse) {
+		const headingToReveal = getHeadings()[request.headingIndex];
+		
+		console.log(headingToReveal);
 
+		headingToReveal.scrollIntoView({
+			block: "start",
+			inline: "nearest"
+		});
+		scrollBy(0, -70); // in case element is covered by floating header
+	}
+}
+
+function getHeadings() {
+	return (
+		Array.from(document.querySelectorAll("h1, h2, h3, h4, h5, h6, h7, h8, h9, h10"))
+		.filter(heading => heading.innerText !== "")
+		.filter(heading => !heading.closest("aside, .sidebar, nav"))
+	);
+}
+
+function getHeadingInfos() {
+	return getHeadings().map(heading => ({
+		level: Number(heading.tagName.slice(1)),
+		innerText: heading.innerText
+	}));
+}
+
+// Initialize
 browser.runtime.onMessage.addListener((request, sender, sendResponse) => {
-    console.log("Received request: ", request);
+	if (request.action) {
+		knownMessages[request.action](request, sender, sendResponse);
+	}
 });
--- Table of contents Extension/Resources/popup.js
+++ Table of contents Extension/Resources/popup.js
@@ -1,1 +1,68 @@
-console.log("Hello World!", browser);
+async function refreshHeadingList() {
+	const headingInfos = await sendMessageToCurrentTab({ action: "getHeadingInfos" });
+	const hasHeadings = headingInfos.length > 0;
+	
+	// Render
+	body.innerHTML = "";
+	
+	const selectElement = document.createElement("select");
+	body.appendChild(selectElement);
+	
+	selectElement.size = Math.min(22, hasHeadings ? headingInfos.length : Number.POSITIVE_INFINITY);
+	selectElement.addEventListener("change", () => {
+		const selectedOption = selectElement.selectedOptions[0];
+		
+		if (selectedOption) {
+			const selectedHeadingIndex = selectedOption.dataset.headingIndex;
+			sendMessageToCurrentTab({ action: "revealHeading", headingIndex: selectedHeadingIndex });
+		}
+	});
+	
+	if (hasHeadings) {
+		// List headings
+		for (let headingIndex = 0; headingIndex < headingInfos.length; headingIndex++) {
+			let headingInfo = headingInfos[headingIndex];
+			
+			const optionElement = document.createElement("option");
+			selectElement.appendChild(optionElement);
+			
+			optionElement.dataset.headingIndex = headingIndex;
+			const indentedHeadingText = "   ".repeat(headingInfo.level - 1) + headingInfo.innerText;
+			optionElement.innerText = indentedHeadingText;
+		}
+	} else {
+		// Show empty state
+		selectElement.style.pointerEvents = "none";
+		
+		// // Pad for vertical alignement
+		const topPaddingAmount = Math.ceil(selectElement.size / 2) - 1;
+		for (let i = 0; i < topPaddingAmount; i++) {
+			selectElement.appendChild(document.createElement("option"));
+		}
+		
+		// // Add message option
+		const optionElement = document.createElement("option");
+		selectElement.appendChild(optionElement);
+		
+		optionElement.innerText = "No heading in document";
+		optionElement.style.textAlign = "center";
+	}
+}
+
+async function sendMessageToCurrentTab(request) {
+	const currentTab = await browser.tabs.getCurrent();
+	return await browser.tabs.sendMessage(currentTab.id, request);
+}
+
+// Initialize
+const body = document.body;
+
+addEventListener("visibilitychange", function() {
+	if (document.visibilityState !== "visible") return;
+	
+	refreshHeadingList();
+});
+
+if (document.visibilityState === "visible") {
+	refreshHeadingList();
+}

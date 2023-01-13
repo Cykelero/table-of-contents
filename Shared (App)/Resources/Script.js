fccd3d1507f8c203e55b4aa12ce21b72c0b01bf4
Nathan Manceaux-Panot <nathan@lemon.garden>
2023-01-13 12:56:59 +0100

    Mention keyboard shortcut in extension description, and Mac app.

--- Shared (App)/Resources/Script.js
+++ Shared (App)/Resources/Script.js
@@ -2,9 +2,9 @@ function show(platform, enabled, useSettingsInsteadOfPreferences) {
     document.body.classList.add(`platform-${platform}`);
 
     if (useSettingsInsteadOfPreferences) {
-        document.getElementsByClassName('platform-mac state-on')[0].innerText = "Table of contents is currently on. You can turn it off in the Extensions section of Safari Settings.";
-        document.getElementsByClassName('platform-mac state-off')[0].innerText = "Table of contents is currently off. You can turn it on in the Extensions section of Safari Settings.";
-        document.getElementsByClassName('platform-mac state-unknown')[0].innerText = "You can turn on Table of contents in the Extensions section of Safari Settings.";
+        document.getElementsByClassName('platform-mac state-on')[0].innerText = "Table of contents is currently on. You can turn it off in the Extensions section of Safari Settings.\n\nTo show the list, click the toolbar button, or press ⌃G.";
+        document.getElementsByClassName('platform-mac state-off')[0].innerText = "Table of contents is currently off. You can turn it on in the Extensions section of Safari Settings.\n\nOnce enabled, show the list using the toolbar button, or by pressing ⌃G.";
+        document.getElementsByClassName('platform-mac state-unknown')[0].innerText = "You can turn on Table of contents in the Extensions section of Safari Settings.\n\nOnce enabled, show the list using the toolbar button, or by pressing ⌃G.";
         document.getElementsByClassName('platform-mac open-preferences')[0].innerText = "Quit and Open Safari Settings…";
     }
 

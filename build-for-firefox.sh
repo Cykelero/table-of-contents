ebc96be7ef94a7336850c305e49ebce865b148b9
Nathan Manceaux-Panot <nathan@lemon.garden>
2024-01-14 10:25:54 +0100

    Firefox: Add build-for-firefox.sh

---
+++ build-for-firefox.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+BASE_SHORTCUT_DESCRIPTION="⌃G"
+FIREFOX_SHORTCUT_DESCRIPTION="⌃G (macOS) or Control+G (Linux, Windows)"
+
+sed -i "" "s/$BASE_SHORTCUT_DESCRIPTION/$FIREFOX_SHORTCUT_DESCRIPTION/g" "Shared (Extension)/Resources/_locales/en/messages.json"
+
+web-ext build -s "Shared (Extension)/Resources" -a "Build/Firefox" --overwrite-dest
+
+sed -i "" "s/$FIREFOX_SHORTCUT_DESCRIPTION/$BASE_SHORTCUT_DESCRIPTION/g" "Shared (Extension)/Resources/_locales/en/messages.json"
